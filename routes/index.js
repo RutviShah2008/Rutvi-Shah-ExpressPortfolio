2f3ad8970556f93436b5748f0ffc1d2c16544690
RutviShah2008 <200390373@student.georgianc.on.ca>
2019-06-23 18:48:54 -0400

    Comments and some layout has been changes

--- routes/index.js
+++ routes/index.js
@@ -5,19 +5,19 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('about', { title: 'Rutvi Shah' });
 });
-
+//get about page
 router.get('/about', (req, res, next) => {
   res.render('about', {title: 'Rutvi Shah'});
 });
-
+//get project page
 router.get('/project', (req, res, next) => {
   res.render('project', {title: 'Have a look at my work!!!'});
 });
-
+//get service page
 router.get('/service', (req, res, next) => {
   res.render('service', {title: 'Services'});
 });
-
+//get contact page
 router.get('/contact', (req, res, next) => {
   res.render('contact', {title: 'ContactMe'});
 });
